0279746eaad6819d6b3d44e394385327f4b5bb05
Marsel <m.gayfiev@dats.team>
2025-09-19 22:11:33 +0300

    symfony init

--- src/Command/UpdateDataCommand.php
+++ src/Command/UpdateDataCommand.php
@@ -15,19 +15,21 @@ readonly class UpdateDataCommand
 
     public function __invoke(): int
     {
-        $mapRequest = $this->client->request(
-            'GET',
-            'https://games-test.datsteam.dev/api/map', [
-            'headers' => [
-                'accept' => 'application/json',
-                'X-Auth-Token' => 'a183405e-8c23-4e4b-afca-7b4a5b115fa8',
-            ],
-        ]);
 
-        file_put_contents('public/map.json', $mapRequest->getContent());
 
         while (true) {
             sleep(1);
+
+            $mapRequest = $this->client->request(
+                'GET',
+                'https://games-test.datsteam.dev/api/map', [
+                'headers' => [
+                    'accept' => 'application/json',
+                    'X-Auth-Token' => 'a183405e-8c23-4e4b-afca-7b4a5b115fa8',
+                ],
+            ]);
+
+            file_put_contents('public/map.json', $mapRequest->getContent());
 
             $scanRequest = $this->client->request(
                 'GET',
